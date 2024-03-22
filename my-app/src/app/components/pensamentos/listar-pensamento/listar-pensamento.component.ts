6bb130280348d5e9e22a1d4c0f3a88f435197c48
antonioln00 <antonioln00@gmail.com>
2024-03-22 14:46:31 -0300

    feat: criando interface de pensamento

--- my-app/src/app/components/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ my-app/src/app/components/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Pensamento } from '../pensamento';
 
 @Component({
   selector: 'app-listar-pensamento',
@@ -6,21 +7,5 @@ import { Component } from '@angular/core';
   styleUrl: './listar-pensamento.component.css'
 })
 export class ListarPensamentoComponent {
-  listaPensamentos = [
-    {
-      conteudo: 'I love angular',
-      autoria: 'Antonio Léo Neto',
-      modelo: 'modelo3'
-    },
-    {
-      conteudo: 'Minha aplicação é decorada com @Input',
-      autoria: 'Antonio Léo Neto',
-      modelo: 'modelo2'
-    },
-    {
-      conteudo: 'Temos de descobrir segurança dentro de nós próprios. Durante o curto espaço de tempo da nossa vida precisamos encontrar o nosso próprio critério de relações com a existência em que participamos tão transitoriamente. Otimismo é esperar pelo melhor. Confiança é saber lidar com o pior.',
-      autoria: 'Boris Pasternak',
-      modelo: 'modelo3'
-    }
-  ];
+  listaPensamentos: Pensamento[] = [];
 }
